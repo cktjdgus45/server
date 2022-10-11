5339ce1d3812406433a8f7e586644bec723b6655
pooh <cktjdgus45@naver.com>
2022-10-11 16:39:13 +0900

    change arguments name 't' to 'tweet'

--- router/tweets.js
+++ router/tweets.js
@@ -26,13 +26,13 @@ let tweets = [
 //Get /tweets?username=:username
 router.get('/', (req, res, next) => {
     const username = req.query.username;
-    const data = username ? tweets.filter(t => t.username === username) : tweets;
+    const data = username ? tweets.filter(tweet => tweet.username === username) : tweets;
     res.status(200).json(data);
 })
 //Get /tweets:id
 router.get('/:id', (req, res, next) => {
     const id = req.params.id;
-    const tweet = tweets.find(t => t.id === id);
+    const tweet = tweets.find(tweet => tweet.id === id);
     if (tweet) {
         res.status(200).json(tweet);
     } else {
@@ -57,7 +57,7 @@ router.post('/', (req, res, next) => {
 router.put('/:id', (req, res, next) => {
     const { id } = req.params;
     const { text } = req.body;
-    const tweet = tweets.find(t => t.id === id);
+    const tweet = tweets.find(tweet => tweet.id === id);
     if (tweet) {
         tweet.text = text;
         res.status(200).json(tweet);
@@ -69,7 +69,7 @@ router.put('/:id', (req, res, next) => {
 //Delete /tweets:id
 router.delete('/:id', (req, res, next) => {
     const { id } = req.params;
-    tweets = tweets.filter(t => t.id !== id);
+    tweets = tweets.filter(tweet => tweet.id !== id);
     res.sendStatus(204);
 });
 export default router;
